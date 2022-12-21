19f7fd2306715ae77f8e1909afadce59a9466715
Jesse Nickles <jesse@littlebizzy.com>
2022-12-22 01:40:43 +0700

    Update multisite-billing-manager.php

--- multisite-billing-manager.php
+++ multisite-billing-manager.php
@@ -1,1 +1,13 @@
-
+<?php 
+/*
+Plugin Name: Multisite Billing Manager
+Plugin URI: https://www.littlebizzy.com/plugins/multisite-billing-manager
+Description: 
+Version: 0.0.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+GitHub Plugin URI: littlebizzy/multisite-billing-manager
+Primary Branch: main
+*/
