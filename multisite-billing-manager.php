1a2f04c3dd8f53453e13740701f001a29fdba63e
Jesse Nickles <jesse@littlebizzy.com>
2022-12-22 01:48:38 +0700

    Update multisite-billing-manager.php

--- multisite-billing-manager.php
+++ multisite-billing-manager.php
@@ -10,4 +10,18 @@ License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 GitHub Plugin URI: littlebizzy/multisite-billing-manager
 Primary Branch: main
+Forked from: https://rudrastyh.com/wordpress-multisite/custom-tabs-with-options.html
 */
+
+add_filter( 'network_edit_site_nav_links', 'multisite_billing_manager_tab' );
+
+function multisite_billing_manager_tab( $tabs ){
+
+        $tabs['billing'] = array(
+                'label' => 'Billing',
+                'url' => 'sites.php?page=billing',
+                'cap' => 'manage_sites'
+        );
+        return $tabs;
+
+}
